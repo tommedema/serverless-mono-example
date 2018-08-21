905f355aeeb44a12ecfa60393346577777628300
Tom Medema <tommedema@gmail.com>
2018-08-21 15:53:01 -0300

    added types package

--- packages/random/src/index.ts
+++ packages/random/src/index.ts
@@ -1,6 +1,8 @@
 export const fetchRandomNumber = async (): Promise<number> => {
   const randomNr = await new Promise<number>(
-    resolve => setTimeout(() => resolve(Math.random()), 500)
+    resolve => setTimeout(() => resolve(
+      Math.random()
+    ), 500)
   )
 
   return randomNr
