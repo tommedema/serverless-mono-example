0973c306b896d42925e188debe98cca2acc024f9
Tom Medema <tommedema@gmail.com>
2018-08-27 16:20:39 -0300

    test git hook

--- packages/sls-random/test/index.ts
+++ packages/sls-random/test/index.ts
@@ -6,7 +6,7 @@ const context = require('./mocks/context.json') as APIGatewayEventRequestContext
 const event = require('./mocks/event.json') as APIGatewayProxyEvent
 
 describe('sls random', () => {
-  it.skip('should fetch a random number', async () => {
+  it('should fetch a random number', async () => {
     const response = await fetchRandomNumber(event, context)
 
     if (response.statusCode !== 200) {
